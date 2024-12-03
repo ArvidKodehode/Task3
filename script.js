842b33c7e02011294948d3ff3b14b7f57edab47b
Arvid Kodehode <arvid.kodehode@gmail.com>
2024-12-03 14:05:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for å håndtere klikk på headeren
+document.getElementById("header").addEventListener("click", function () {
+    console.log("🎉 Gratulerer! Du fant Easter egget! 🐣");
+    alert("🎉 Gratulerer! Du fant Easter egget! 🐣");
+});
